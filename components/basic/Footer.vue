49b887b7f3b3ddc37267b34e22d3cd838ff1cc8b
sasha <aka.kisler@gmail.com>
2021-02-23 00:51:31 +0500

    html tags

--- components/basic/Footer.vue
+++ components/basic/Footer.vue
@@ -1,12 +1,33 @@
 <template>
   <footer class="footer">
-
+    <div class="reference-information">
+      <div class="social-network helpful-item">
+        <h4></h4>
+        <div class="icon-list">
+        </div>
+      </div>
+      <div class="footer-logo">
+      </div>
+      <div class="contacts helpful-item">
+        <h4></h4>
+        <div class="phone-list">
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
 <script>
 export default {
-  name: "Footer"
+  name: "Footer",
+  data() {
+    return {
+      loading: false,
+      icon_list:[{}],
+      phone_list:[{}],
+    }
+  },
+
 }
 </script>
 
