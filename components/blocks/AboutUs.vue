9c8375c0532ca25466b48db83e0aa19c08ddafc4
sasha <aka.kisler@gmail.com>
2021-02-28 16:20:53 +0500

    add styles for AboutUs cjmponent

--- components/blocks/AboutUs.vue
+++ components/blocks/AboutUs.vue
@@ -1,6 +1,12 @@
 <template>
 <section class="about-us" id="about-us">
+  <div class="au-container">
 
+  </div>
+  <div class="au-line">
+    <h3>О НАС</h3>
+    <div class="line"></div>
+  </div>
 </section>
 </template>
 
