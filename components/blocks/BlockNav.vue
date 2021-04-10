3ab5df370cfef35fd80bb2ea1b122a978fb0ce08
sasha <aka.kisler@gmail.com>
2021-04-10 23:37:46 +0500

    more update styles, add content, edit html

--- components/blocks/BlockNav.vue
+++ components/blocks/BlockNav.vue
@@ -2,13 +2,20 @@
   <section class="block-nav" id="block-nav">
     <div class="col-left">
       <div class="nav-item-1-1">
-        <h1>Плантеа Баскетбола</h1>
+        <h1>Планета <br> баскетбола</h1>
+        <img src="~assets/image/block-basketball.svg" class="block-img" alt="aa">
       </div>
-      <div class="nav-item-1-2"></div>
+      <div class="nav-item-1-2">
+        <h1>ЗАПИСЬ НА <br>
+          ТРЕНИРОВКУ</h1>
+        <img src="~/assets/image/icons/icon-click.svg" alt="click" width="111" height="111">
+      </div>
     </div>
     <div class="col-center">
       <div class="nav-row">
         <div class="nav-item-2-1">
+          <h2>Наша <br> команда</h2>
+          <img src="~/assets/image/icon-team2.svg"  class="block-img" alt="icon-team">
 
         </div>
         <div class="nav-cross">
@@ -16,7 +23,9 @@
         </div>
       </div>
       <div class="nav-item-2-2">
-
+        <h1>ИНФОРМАЦИЯ <br>
+          О ТРЕНИРОВКАХ</h1>
+        <img src="~assets/image/block-info.svg" class="block-img" alt="aa">
       </div>
       <div class="nav-row">
         <div class="nav-item-h-line"></div>
